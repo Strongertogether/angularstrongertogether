b8142e8dbc428f11cb612cc5d5499b497d6aaea7
chalscc <carlescatalancalabuig@gmail.com>
2017-02-01 21:54:40 +0100

    dynamic google maps

--- src/client/app/hospitals/hospitals.controller.js
+++ src/client/app/hospitals/hospitals.controller.js
@@ -16,6 +16,7 @@
         vm.numPerPage = 5;
         vm.maxSize = 5;
         vm.currentPage = 1;
+        vm.markers = [];
 
         $scope.$watch(update);
         // var watcher = vm.watch('./technicians.html');
@@ -35,6 +36,7 @@
         function getHospitals(){
           return dataservice.getHospitals().then(function (data) {
             vm.hospitals = data;
+            getMarkers(vm.hospitals);
             return vm.hospitals;
           });
         }
@@ -43,5 +45,70 @@
             var begin = ((vm.currentPage - 1) * vm.numPerPage), end = begin + vm.numPerPage;
             vm.filteredHospitals = vm.hospitals.slice(begin, end);
         }
+
+       function getMarkers(hospitals) {
+
+          for (var i = 0; i < hospitals.length; i++) {
+            var latitude = hospitals[i].latitude;
+            var longitude = hospitals[i].longitude;
+
+            vm.Marker = {
+              coords: {
+                  latitude:latitude,
+                  longitude:longitude
+                },
+              id:i
+            }
+
+            vm.markers.push(vm.Marker);
+
+          }
+
+        }
+
+
+        /*
+                    vm.Marker = {
+                      coords: {
+                          latitude:latitude,
+                          longitude:longitude
+                        },
+                      id:i
+                    }
+
+                    vm.markers.push(vm.Marker);
+          */
+
+
+        /*
+         * Test marker google maps
+         *
+
+            $scope.marker = {
+              id: 0,
+
+              latitude: 38.8101561,
+              longitude: -0.6043774
+
+              options: { draggable: true },
+              events: {
+                dragend: function (marker, eventName, args) {
+                  $log.log('marker dragend');
+                  var lat = marker.getPosition().lat();
+                  var lon = marker.getPosition().lng();
+                  $log.log(lat);
+                  $log.log(lon);
+
+                  $scope.marker.options = {
+                    draggable: true,
+                    labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+                    labelAnchor: "100 0",
+                    labelClass: "marker-labels"
+                  };
+                }
+              }
+            };
+*/
+
     }
 })();
